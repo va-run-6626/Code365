847f7c0b9a648f02dfbcf9b043af591035c47756
BHAVUK GAUR <bhavukgaur.bkbiet25@gmail.com>
2024-04-13 23:54:54 +0530

    Day_13_TaskSchedular_Varun

---
+++ src/Day_13/Varun/TaskSchedular.java
@@ -0,0 +1,28 @@
+package Day_13.Varun;
+
+import java.util.Arrays;
+
+public class TaskSchedular {
+    public static void main(String[] args) {
+        char[] tasks = {'A','A','A','B','B','B'};
+        int n = 2;
+        int res = new Solution().leastInterval(tasks,n);
+ }
+}
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+        Arrays.sort(freq);
+        int chunk = freq[25] - 1;
+        int idle = chunk * n;
+
+        for (int i = 24; i >= 0; i--) {
+            idle -= Math.min(chunk, freq[i]);
+        }
+
+        return idle < 0 ? tasks.length : tasks.length + idle;
+    }
+}
