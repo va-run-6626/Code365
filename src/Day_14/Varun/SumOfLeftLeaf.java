d020a3d72583a7e90a2c6ea7ccfe4c2059dad1bf
va-run-6626 <vbhutada143@gmail.com>
2024-04-14 23:24:39 +0530

    Day14_SumOfLeftLeaves_Varun

---
+++ src/Day_14/Varun/SumOfLeftLeaf.java
@@ -0,0 +1,43 @@
+package Day_14.Varun;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class SumOfLeftLeaf {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode A = new TreeNode(9);
+        TreeNode B = new TreeNode(20);
+        TreeNode C = new TreeNode(15);
+        TreeNode D = new TreeNode(7);
+
+        root.left = A;
+        root.right = B;
+        B.left = C;
+        B.right = D;
+        System.out.println(new Solution().sumOfLeftLeaves(root));
+    }
+}
+
+class Solution{
+    public int sumOfLeftLeaves(TreeNode root){
+        return helper(root,false);
+    }
+    public int helper(TreeNode root, boolean isLeaf){
+        if(root == null) return 0;
+        if(root.left == null && root.right == null){
+            return isLeaf ? root.val : 0;
+        }
+        return helper(root.left,true)+helper(root.right,false);
+    }
+}
