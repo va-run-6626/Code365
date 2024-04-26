ccee4336b1dd7f5a82fa67ee17868f76bc790bd3
va-run-6626 <vbhutada143@gmail.com>
2024-04-26 18:50:03 +0530

    Day26_MinimumFallingPathSumII_Varun

---
+++ src/Day_26/Varun/MinimumFallingPathSum_II.java
@@ -0,0 +1,42 @@
+package Day_26.Varun;
+import java.util.*;
+public class MinimumFallingPathSum_II {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},{4,5,6},{7,8,9}
+        };
+        int minPathSum = minFallingPathSum(matrix);
+        System.out.println(minPathSum);
+    }
+    public  static int minFallingPathSum(int[][] grid) {
+        int n = grid.length, res = Integer.MAX_VALUE;
+        int[][] dp = new int[n][n];
+        for(int row[] : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        for(int j = 0; j < n; ++j) {
+            dp[0][j] = grid[0][j];
+        }
+
+        for(int i = 1; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                int temp = Integer.MAX_VALUE;
+
+                for(int k = 0; k < n; ++k) {
+                    if(j != k) {
+                        temp = Math.min(temp, grid[i][j] + dp[i - 1][k]);
+                    }
+
+                    dp[i][j] = temp;
+                }
+            }
+        }
+
+        for(int j = 0; j < n; ++j) {
+            res = Math.min(res, dp[n - 1][j]);
+        }
+
+        return res;
+    }
+}
