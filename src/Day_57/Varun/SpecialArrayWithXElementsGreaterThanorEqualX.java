26d2640a86e27aa290ee05e5947497b739abf806
va-run-6626 <vbhutada143@gmail.com>
2024-05-28 08:43:12 +0530

    Using Pure Binary Search

--- src/Day_57/Varun/SpecialArrayWithXElementsGreaterThanorEqualX.java
+++ src/Day_57/Varun/SpecialArrayWithXElementsGreaterThanorEqualX.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class SpecialArrayWithXElementsGreaterThanorEqualX {
     public static void main(String[] args) {
-        int[] nums = {3,6,7,7,0};
-        int x = new Solution().specialArray(nums);
-        System.out.println(x);
+        int[] nums = {0,0};
+        int x1 = new Solution1().specialArray(nums);
+        System.out.println(x1);
+        int x2 = new Solution2().specialArray(nums);
+        System.out.println(x2);
     }
 }
-class Solution {
+class Solution1 {
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
         int s = 0;
@@ -29,5 +31,38 @@ class Solution {
             if(i >= mid) cnt++;
         }
         return cnt;
+    }
+}
+class Solution2 {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int s = 0;
+        int e = nums.length;
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            int ans = cntNums(mid, nums);
+            if(ans == -1) return ans;
+            if(ans == mid) return ans;
+            else if(ans > mid) s = mid +1;
+            else e = mid -1;
+        }
+        return -1;
+    }
+    private int cntNums(int target, int[] nums){
+        int ans = 0;
+        int s = 0;
+        int e = nums.length-1;
+        int potAns = -1;
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            if(nums[mid] >= target){
+                potAns = mid;
+                e = mid-1;
+            }else{
+                s = mid+1;
+            }
+        }
+        if(potAns == -1) return potAns;
+        return nums.length - potAns;
     }
 }
