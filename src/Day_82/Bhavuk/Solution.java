bfbb942413b7ed616c8db90ab96d01ea30e59fa1
BHAVUK GAUR <bhavukgaur.bkbiet25@gmail.com>
2024-06-25 00:13:44 +0530

    Origin main

---
+++ src/Day_82/Bhavuk/Solution.java
@@ -0,0 +1,28 @@
+package Day_82.Bhavuk;
+
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        int n = nums.length;
+
+        int flips = 0;
+        boolean[] isFlipped = new boolean[n];
+        int flipCounti = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i >= k && isFlipped[i - k]) {
+                flipCounti--;
+            }
+
+            if (flipCounti % 2 == nums[i]) {
+                if (i + k > n) {
+                    return -1;
+                }
+                flipCounti++;
+                isFlipped[i] = true;
+                flips++;
+            }
+        }
+
+        return flips;
+    }
+}
